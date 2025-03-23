f22870832f2d3160206db0c0967c9333bc97166b
abhishek <abhi.verma9492@gmail.com>
2025-03-24 01:58:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { Route, Routes } from "react-router-dom";
+
+import Sidebar from "./components/common/Sidebar";
+
+import OverviewPage from "./pages/OverviewPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
+import { useEffect, useState } from "react";
+import { addPunchData, getPunchData } from "./Data/Data";
+const WS_URL = "wss://aws-production-4ef1.up.railway.app";
+
+function App() {
+	const [message, setMessage] = useState("");
+	const [socket, setSocket] = useState(null);
+	const [punchData, setPunchData] = useState({});
+
+	useEffect(() => {
+		const ws = new WebSocket(WS_URL);
+		setSocket(ws);
+
+		ws.onopen = () => console.log("Connected to WebSocket");
+		ws.onmessage = (event) => {
+			try {
+				const newData = JSON.parse(event.data); // Assuming the message is in JSON format
+				setMessage(newData);
+				const value = [
+					newData.data["Punch power (N)"],
+					newData.data["Punch Speed (km/h)"],
+					newData.data["Reflex time (ms)"],
+					newData.data["isblocked"],
+					newData.data["timestamp"].substring(11, 19),
+				];
+				addPunchData(newData.data.timestamp.substring(0, 11), value);
+				setPunchData(getPunchData());
+			} catch (error) {
+				console.error("Error parsing WebSocket message", error);
+			}
+
+			ws.onclose = () => {
+				console.log("WebSocket Disconnected");
+			};
+
+			ws.onerror = (error) => {
+				console.error("WebSocket Error:", error);
+			};
+
+			// Cleanup function to close WebSocket when component unmounts
+			return () => {
+				ws.close();
+			};
+		};
+
+		return () => ws.close();
+	}, []);
+
+	// console.log(message.data["timestamp"]);
+
+	return (
+		<div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+			{/* BG
+			<div className='fixed inset-0 z-0'>
+				<div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
+				<div className='absolute inset-0 backdrop-blur-sm' />
+			</div> */}
+			{message ? (
+				<>
+					<Sidebar />
+					<Routes>
+						<Route path='/' element={<OverviewPage punch={[
+							message.data["timestamp"].substring(0, 11),
+							message.data["timestamp"].substring(11, 19),
+							message.data["Punch power (N)"],
+							message.data["Punch Speed (km/h)"],
+							message.data["Reflex time (ms)"],
+							message.data["isblocked"] === 0 ? "No" : "Yes",
+						]}
+							punchData={punchData} />} />
+						<Route path='/analytics' element={<AnalyticsPage />} />
+						<Route path='/settings' element={<SettingsPage />} />
+					</Routes></>
+			) : (
+				<p>Waiting for data...</p>
+			)}
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,70 @@
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import { motion } from "framer-motion";
+
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import SalesOverviewChart from "../components/overview/SalesOverviewChart";
+
+const OverviewPage = (props) => {
+	const power = [];
+	const speed = [];
+	const accuracy = [];
+	// console.log(props.punch[0].substring(0, 19));
+	for (const [key, value] of Object.entries(props.punchData)) {
+
+		// console.log(props.punch[0]);
+
+		let totalPunches = 0;
+		let blockedPunches = 0;
+		value.forEach((element) => {
+
+
+			power.push({ "date": `${props.punch[0]} ${element[4]}`, "power": element[0] });
+			speed.push({ "date": `${props.punch[0]} ${element[4]}`, "speed": element[1] });
+			if (element[3] === 0) {
+				blockedPunches += 1;
+			}
+			totalPunches += 1;
+
+
+		});
+		// const accuracyExist = false;
+		// accuracy.forEach(element => {
+		// 	console.log(element);
+
+
+		// });
+		accuracy.push((100 - (blockedPunches / totalPunches) * 100).toFixed(2));
+	}
+	// console.log(props);
+
+	return (
+		<div className='flex-1 overflow-auto relative z-10'>
+			<Header title='Overview' />
+
+			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+				{/* STATS */}
+				<motion.div
+					className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 1 }}
+				>
+					<StatCard name='Punch Power (N)' icon={Zap} value={props.punch[2]} color='#6366F1' />
+					<StatCard name='Punch Speed (km/h)' icon={Users} value={props.punch[3]} color='#8B5CF6' />
+					<StatCard name='Reflex Time (ms)' icon={ShoppingBag} value={props.punch[4]} color='#EC4899' />
+					<StatCard name='Blocked' icon={BarChart2} value={props.punch[5]} color='#10B981' />
+				</motion.div>
+
+				{/* CHARTS */}
+
+				<div className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+					<SalesOverviewChart data={power} heading={"Punch Power (N)"} xkey={"date"} ykey={"power"} stroke={'#8B5CF6'} fill={'#8B5CF6'} />
+					<SalesOverviewChart data={speed} heading={"Punch Speed (km/h)"} xkey={"date"} ykey={"speed"} stroke={'#10B981'} fill={'#10B981'} />
+					{/* <SalesOverviewChart data={power} heading={"Punch Power"} xkey={"date"} ykey={"power"} /> */}
+				</div>
+			</main>
+		</div>
+	);
+};
+export default OverviewPage;
